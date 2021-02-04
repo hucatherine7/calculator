1fc750ba602977dc5935f12727abdaeb890251a5
hucatherine7 <hucatherine7@gmail.com>
2021-02-04 08:22:44 -0800

    Adding basic code without unit tests yet

---
+++ calculator.py
@@ -0,0 +1,15 @@
+
+
+def main():
+    input1 = int(input("Enter an integer: "))
+    input2 = int(input("Enter another integer: "))
+
+    print("\nHere are your results:")
+    print(input1, "plus", input2, "equals", input1+input2)
+    print(input1, "minus", input2, "equals", input1-input2)
+    print(input1, "divided by", input2, "equals", input1/input2)
+    print(input1, "times", input2, "equals", input1*input2)
+
+
+
+main()
